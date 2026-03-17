5b87f73162b9af33259b3b9b52dac716b3b57cf1
Poloqwq <fuxang.yai@gmail.com>
2026-03-18 00:28:19 +0800

    Initial commit

---
+++ src/main/java/net/poloqwq/modlearning/ModLearningClient.java
@@ -0,0 +1,10 @@
+package net.poloqwq.modlearning;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModLearningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/poloqwq/modlearning/ModLearningDataGenerator.java
@@ -0,0 +1,11 @@
+package net.poloqwq.modlearning;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModLearningDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
