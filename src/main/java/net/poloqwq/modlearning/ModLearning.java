9827de2835c6428ff76e9f79abc2f6b8daa35cc5
Poloqwq <fuxang.yai@gmail.com>
2026-03-18 13:01:50 +0800

    feat: adding two new items, pink_garnet and raw_pink_garnet.

--- src/main/java/net/poloqwq/modlearning/ModLearning.java
+++ src/main/java/net/poloqwq/modlearning/ModLearning.java
@@ -2,6 +2,7 @@ package net.poloqwq.modlearning;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.poloqwq.modlearning.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +21,7 @@ public class ModLearning implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 
 
 	}
